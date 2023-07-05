8695130ce8fa25eb4591fc4927567d1585e609ba
SabariramSG <ramsabari1180@gmail.com>
2023-07-05 22:19:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cvault/CardPreviewFragment.kt
@@ -0,0 +1,41 @@
+package com.example.cvault
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cvault.databinding.FragmentCardPreviewBinding
+
+class CardPreviewFragment : Fragment() {
+
+    private var _binding: FragmentCardPreviewBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentCardPreviewBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpUI()
+    }
+
+    private fun setUpUI() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
